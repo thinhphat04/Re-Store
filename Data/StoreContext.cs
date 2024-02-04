831119b95d8773ba23e104819642c217d36f7ff8
ngothinhphat <ngothinhphat244@gmail.com>
2024-02-04 22:04:18 +0700

    setting project

---
+++ Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Re_Store.Entities;
+
+namespace Re_Store.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Product> Products { get; set; }
+    
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,19 @@
+namespace Re_Store.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    public long Price { get; set; }
+    
+    public string PictureUrl { get; set; }
+    
+    public string Type { get; set; }
+    
+    public string Brand { get; set; }
+    
+    public int Quantity { get; set; }
+}
