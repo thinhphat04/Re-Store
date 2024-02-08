a5b4d2e28d822773d172c30bdbb72fe39bfdb0e7
ngothinhphat <ngothinhphat244@gmail.com>
2024-02-09 00:23:08 +0700

    update async ProductsController.cs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Re_Store.Data;
 using Re_Store.Entities;
 
@@ -9,29 +10,29 @@ namespace Re_Store.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext db;
+        private readonly StoreContext _db;
         public ProductsController(StoreContext db)
         {
-            this.db = db;
+            this._db = db;
         }
         
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
-            var products = db.Products.ToList();
+            var products = await _db.Products.ToListAsync();
             return products;
         }
-        
+
         [HttpGet("{id}")]
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = db.Products.Find(id);
+            var product = await _db.Products.FindAsync(id);
             if (product == null)
             {
                 return NotFound();
             }
+
             return product;
         }
-        
     }
 }
